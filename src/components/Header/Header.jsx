5d71e3cfba42d644048f28345a0ceb50a4275e14
AdrianoRama <ramaadriano@outlook.com>
2022-01-05 01:22:34 +0100

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import LeftHeader from './LeftHeader'
+import Search from './Search'
+
+function Header() {
+    return (
+        <header className="header">
+            <LeftHeader />
+            <Search />
+        </header>
+    )
+}
+export default Header
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import Content from "./Content"
+import Toolbar from "./Toolbar"
+
+
+function Main() {
+    return (<main className="email-view">
+        <Toolbar />
+        <Content />
+    </main>
+    )
+}
+
+export default Main
